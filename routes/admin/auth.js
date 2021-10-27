d708de884b15253fedce8cf1fc9b5d8d024d16ea
Michael Loughman <mcloughman@gmail.com>
2021-10-27 14:03:02 -0700

    Create validators file and move validator logic to that file...savin as methods on the module exports object. This really cleans up the the post routecode.

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -4,7 +4,11 @@ const { check, validationResult } = require("express-validator"); // destructure
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
 const signinTemplate = require("../../views/admin/auth/signin");
-
+const {
+  requireEmail,
+  requirePassword,
+  requirePasswordConfirmation,
+} = require("./validators");
 // we will create a sub-router and export it
 const router = express.Router();
 
@@ -15,32 +19,7 @@ router.get("/signup", (req, res) => {
 // and now we need to actually make use of the middleware we created
 router.post(
   "/signup",
-  [
-    check("email")
-      .trim()
-      .normalizeEmail()
-      .isEmail()
-      .withMessage("Must be a valid email")
-      .custom(async (email) => {
-        const existingUser = await usersRepo.getOneBy({ email });
-        if (existingUser) {
-          throw new Error("Email already in use");
-        }
-      }),
-    check("password")
-      .trim()
-      .isLength({ min: 4, max: 20 })
-      .withMessage("Must be between 4 and 20 characters"),
-    check("passwordConfirmation")
-      .trim()
-      .isLength({ min: 4, max: 20 })
-      .withMessage("Must be between 4 and 20 characters")
-      .custom((passwordConfirmation, { req }) => {
-        if (passwordConfirmation !== req.body.password) {
-          throw new Error("Passwords must match");
-        }
-      }),
-  ],
+  [requireEmail, requirePassword, requirePasswordConfirmation],
 
   async (req, res) => {
     const errors = validationResult(req);
