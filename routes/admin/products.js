1809cb079faff482129fa9bafe4c8f581926735d
Michael Loughman <mcloughman@gmail.com>
2021-10-27 22:22:14 -0700

    Add multer middleware  to facilitate handling of the input type='file'. bodyParser will only work on urlencoded inputs. Not compatible with type='file'. we add the ectype attribute and set it to 'multipart/form-data'

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const { validationResult } = require("express-validator");
+const multer = require("multer");
+
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
 const { requireTitle, requirePrice } = require("./validators");
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/admin/products", (req, res) => {});
 
@@ -12,10 +15,17 @@ router.get("/admin/products/new", (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
-router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
-  res.send("Submitted");
-  const errors = validationResult(req);
-  console.log(errors);
-});
+router.post(
+  "/admin/products/new",
+  [requireTitle, requirePrice],
+  upload.single("image"),
+  async (req, res) => {
+    res.send("Submitted");
+    const errors = validationResult(req);
+    const image = req.file.buffer.toString("base64");
+    const { title, price } = req.body;
+    await productsRepo.create({ title, price, image });
+  }
+);
 
 module.exports = router;
